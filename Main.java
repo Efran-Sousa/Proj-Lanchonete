a5496c8aee2fd25ac83baf161fb6346ad62f51e6
Efran Santos Sousa <efrangameboy@outlook.com>
2025-03-21 14:53:18 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,11 +1,8 @@
-import gui.MainFrame;
-import javax.swing.SwingUtilities;
+import ui.MenuController;
 
 public class Main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            MainFrame frame = new MainFrame();
-            frame.setVisible(true);
-        });
+        MenuController controller = new MenuController();
+        controller.iniciar();
     }
 }
